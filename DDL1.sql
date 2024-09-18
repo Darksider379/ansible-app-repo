9a0777101eb0d12fcfadeceb3647507aa23ece5e
Chandra Mukherjee <89653407+Darksider379@users.noreply.github.com>
2024-09-18 17:41:23 +0000

    Added code

---
+++ DDL1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "table" (
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT
+);
---
+++ DDL2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "table2" (
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT
+);
